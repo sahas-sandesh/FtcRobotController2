4b603b53e273108113fc47801c609ffc215dda7f
sahas-sandesh <sahassandesh@gmail.com>
2023-09-03 17:24:43 +0530

    Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SSMTest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SSMTest1.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-@TeleOp(name="F_TeleOp")
+@TeleOp(name="One_TeleOp")
 public class SSMTest1 extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {

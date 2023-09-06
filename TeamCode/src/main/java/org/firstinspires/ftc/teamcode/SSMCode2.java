b6c63af50f68c6df7c116b33bb7b6b2c065fc83e
sahas-sandesh <sahassandesh@gmail.com>
2023-09-06 23:02:56 +0530

    Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SSMCode2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SSMCode2.java
@@ -15,35 +15,20 @@ public class SSMCode2 extends LinearOpMode {
         DcMotor FR;
         DcMotor BL;
         DcMotor BR;
-        FL = hardwareMap.get(DcMotor.class,"SSMmotor1");
-        FR = hardwareMap.get(DcMotor.class,"SSMmotor2");
-        BL = hardwareMap.get(DcMotor.class,"SSMmotor3");
-        BR = hardwareMap.get(DcMotor.class,"SSMmotor4");
+        FL = hardwareMap.get(DcMotor.class,"SS_motor1");
+        FR = hardwareMap.get(DcMotor.class,"SS_motor2");
+        BL = hardwareMap.get(DcMotor.class,"SS_motor3");
+        BR = hardwareMap.get(DcMotor.class,"SS_motor4");
         waitForStart();
 
         while(opModeIsActive()){
             double speed = gamepad1.left_stick_x;
             FL.setPower(speed);
-            FR.setPower(speed);
-            BL.setPower(speed*-1);
+            FR.setPower(speed*-1);
+            BL.setPower(speed);
             BR.setPower(speed*-1);
 
-        DcMotor SSMmotor1;
-        DcMotor SSMmotor2;
-        DcMotor SSMmotor3;
-        DcMotor SSMmotor4;
-        SSMmotor1 = hardwareMap.get(DcMotor.class,"SSMmotor1");
-        SSMmotor2 = hardwareMap.get(DcMotor.class,"SSMmotor2");
-        SSMmotor3 = hardwareMap.get(DcMotor.class,"SSMmotor3");
-        SSMmotor4 = hardwareMap.get(DcMotor.class,"SSMmotor4");
-        waitForStart();
 
-        while(opModeIsActive()){
-            double speed = gamepad1.left_stick_y;
-            SSMmotor1.setPower(speed);
-            SSMmotor2.setPower(speed);
-            SSMmotor3.setPower(speed);
-            SSMmotor4.setPower(speed);
 
 
         }
